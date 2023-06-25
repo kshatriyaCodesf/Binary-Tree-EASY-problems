1daa63f4e8ff317986953701d258b8143fdfdf60
kshatriyaCodesf <134126613+kshatriyaCodesf@users.noreply.github.com>
2023-06-25 12:51:11 +0530

    Time: 19 ms (31.29%), Space: 15.1 MB (17.44%) - LeetHub

--- 2331-evaluate-boolean-binary-tree/2331-evaluate-boolean-binary-tree.cpp
+++ 2331-evaluate-boolean-binary-tree/2331-evaluate-boolean-binary-tree.cpp
@@ -12,33 +12,33 @@
 class Solution {
 public:
     //method 1:(20ms)
-    // bool value ;
-    // bool evaluateTree(TreeNode* root) 
-    // {
-    //     if(root->val == 1 || root->val == 0)
-    //     {
-    //     	return root->val; 
-    //     }
-    //     if(root->val==2)
-    //     {
-    //     	value =  evaluateTree(root->right)||evaluateTree(root->left);
-    //     }
-    //     else if(root->val==3)
-    //     {
-    //     	value = evaluateTree(root->right) && evaluateTree(root->left);
-    //     }
-    //     return value ;
-    // }
+    bool value ;
+    bool evaluateTree(TreeNode* root) 
+    {
+        if(root->val == 1 || root->val == 0)
+        {
+        	return root->val; 
+        }
+        if(root->val==2)
+        {
+        	value =  evaluateTree(root->right)||evaluateTree(root->left);
+        }
+        else if(root->val==3)
+        {
+        	value = evaluateTree(root->right) && evaluateTree(root->left);
+        }
+        return value ;
+    }
     
     //method 2:
-    bool evaluateTree(TreeNode* root) {
-        if( root->val == 1)return true;
-        if( root->val ==0)return false;
-        if (root->val ==2)return evaluateTree(root->left) or evaluateTree(root->right);
-        if(root->val ==3)return evaluateTree(root->left) and evaluateTree (root->right);
+//         bool evaluateTree(TreeNode* root) {
+//         if( root->val == 1)return true;
+//         if( root->val ==0)return false;
+//         if (root->val ==2)return evaluateTree(root->left) or evaluateTree(root->right);
+//         if(root->val ==3)return evaluateTree(root->left) and evaluateTree (root->right);
 
-        return true;
+//         return true;
 
-        
-    }
+    // }
+    
 };

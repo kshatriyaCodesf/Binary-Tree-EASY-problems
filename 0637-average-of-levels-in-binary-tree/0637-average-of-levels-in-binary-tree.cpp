06f928bbf9f2c5ebfc646fa2d33cf69df986fc56
kshatriyaCodesf <134126613+kshatriyaCodesf@users.noreply.github.com>
2023-07-19 03:51:04 +0530

    Time: 15 ms (78.08%), Space: 22.5 MB (48.26%) - LeetHub

---
+++ 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<double> averageOfLevels(TreeNode* root) {
+        vector<double> vec;
+        queue<TreeNode*> q;
+
+        q.push(root);
+        q.push(nullptr); // Add nullptr as a level separator
+
+        while (!q.empty()) {
+            double sum = 0;
+            int count = 0;
+            
+            while (q.front() != nullptr) {
+                TreeNode* node = q.front();
+                q.pop();
+                
+                sum += node->val;
+                count++;
+                
+                if (node->left != nullptr) {
+                    q.push(node->left);
+                }
+                if (node->right != nullptr) {
+                    q.push(node->right);
+                }
+            }
+            
+            q.pop(); // Remove the level separator nullptr
+            
+            if (count > 0) {
+                double average = sum / count;
+                vec.push_back(average);
+            }
+            
+            if (!q.empty()) {
+                q.push(nullptr); // Add the level separator for the next level
+            }
+        }
+        
+        return vec;
+    }
+};

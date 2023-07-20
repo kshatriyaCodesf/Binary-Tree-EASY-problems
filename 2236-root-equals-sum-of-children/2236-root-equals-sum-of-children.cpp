a65444c5eb966110577f7003f27e1fed6339858a
kshatriyaCodesf <134126613+kshatriyaCodesf@users.noreply.github.com>
2023-07-21 01:47:57 +0530

    Time: 3 ms (66.66%), Space: 12.4 MB (98.06%) - LeetHub

--- 2236-root-equals-sum-of-children/2236-root-equals-sum-of-children.cpp
+++ 2236-root-equals-sum-of-children/2236-root-equals-sum-of-children.cpp
@@ -1,17 +1,35 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
 class Solution {
 public:
-    bool checkTree(TreeNode* root) {
-        return root->val == root->left->val+root->right->val;
+   
+/*only use in the case of the multiple nodes not the three nodes*/
+    
+//     bool z = true;
+//     int helper(TreeNode*root)
+//     {
+//         if(root==NULL){return 0;}
+//         if(root->left==NULL and root->right==NULL){return root->val;}
+        
+//         int left = helper(root->left);
+//         int right = helper(root->right);
+        
+//         if(root->val!=left+right){z=false;}
+//         return root->val;
+    
+//      }
+//     bool checkTree(TreeNode* root) 
+    // {
+//         if(root==NULL){return false;}
+//         int x = helper(root);
+//         return z ;
+    // }   
+    
+    
+    
+    bool checkTree(TreeNode*root)
+    {
+        if(root->left->val+root->right->val==root->val){return true;}
+        else{return false;}
     }
+ 
 };

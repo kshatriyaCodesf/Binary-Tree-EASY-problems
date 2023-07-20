8706cd53c28e14d1c8ce7489624b0a774e93dc10
kshatriyaCodesf <134126613+kshatriyaCodesf@users.noreply.github.com>
2023-07-21 01:21:54 +0530

    Time: 7 ms (98.42%), Space: 14.8 MB (99.27%) - LeetHub

--- 2331-evaluate-boolean-binary-tree/2331-evaluate-boolean-binary-tree.cpp
+++ 2331-evaluate-boolean-binary-tree/2331-evaluate-boolean-binary-tree.cpp
@@ -1,44 +1,27 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
 class Solution {
 public:
-    //method 1:(20ms)
-    bool value ;
     bool evaluateTree(TreeNode* root) 
     {
-        if(root->val == 1 || root->val == 0)
-        {
-        	return root->val; 
-        }
-        if(root->val==2)
-        {
-        	value =  evaluateTree(root->right)||evaluateTree(root->left);
-        }
-        else if(root->val==3)
-        {
-        	value = evaluateTree(root->right) && evaluateTree(root->left);
-        }
-        return value ;
+//         if(root==NULL)
+//         {
+//             return false;
+//         }
+//         if(root->left==NULL and root->right==NULL){return root->val;}
+//         bool left = evaluateTree(root->left);
+//         bool right = evaluateTree(root->right);
+        
+//         if(root->val==2){return left or right;}
+//         else if(root->val==3){return left and right;}
+        
+//         return false ; // this is for the those cases in which root->val is neither 2 or nor 3
+           
+        
+         
+        if (root->val < 2) return root->val;
+        
+        else if (root->val == 2) return evaluateTree(root->left) || evaluateTree(root->right);
+        
+        return evaluateTree(root->left) && evaluateTree(root->right);
     }
-    
-    //method 2:
-//         bool evaluateTree(TreeNode* root) {
-//         if( root->val == 1)return true;
-//         if( root->val ==0)return false;
-//         if (root->val ==2)return evaluateTree(root->left) or evaluateTree(root->right);
-//         if(root->val ==3)return evaluateTree(root->left) and evaluateTree (root->right);
-
-//         return true;
-
-    // }
-    
 };

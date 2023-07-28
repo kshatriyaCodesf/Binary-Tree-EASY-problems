d5ed3d26f95151d247fe2859c786ca0732c83144
kshatriyaCodesf <134126613+kshatriyaCodesf@users.noreply.github.com>
2023-07-28 18:36:15 +0530

    Time: 434 ms (99.96%), Space: 163.9 MB (84.42%) - LeetHub

--- 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.cpp
+++ 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.cpp
@@ -1,37 +1,23 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
+
 
 class Solution {
 public:
-    TreeNode*z = NULL ;
+    TreeNode*z=NULL;
     void helper(TreeNode*cloned , TreeNode*target)
     {
-    		if(cloned == NULL)
-    		{
-    			return ;
-    		}
-    		
-    		if(target->val == cloned->val)
-    		{
-    			z = cloned ;
-    			return ;
-    		}
-    		helper(cloned->left , target);
-    		helper(cloned->right , target);
-    		
+        if(cloned==NULL){return ;}
+        if(cloned->val == target->val)
+        {
+            z = cloned;
+            return ;
+        }
+        helper(cloned->left,target);
+        helper(cloned->right,target);
     }
-    
-    
     TreeNode* getTargetCopy(TreeNode* original, TreeNode* cloned, TreeNode* target) {
-    	  helper(cloned , target);
-    	  return z ;
-       
+      if(original==NULL){return NULL;}
+        
+       helper(cloned  , target);
+        return z ;
     }
 };
